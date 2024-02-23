c58a41948e91353a7fc700877ba43f061a507b91
manuhostin <hostimmanu@gmail.com>
2024-02-23 14:55:55 -0300

    atualizações da aula de hoje.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0)
+
+function decrementar () {
+  contador.value--
+}
+
+</script>
+
+<template> 
+<button @click="contador++">+</button>
+<button @click="decrementar">-</button>
+<p>valor do contador: {{ contador }}</p>
+</template>
+
+<style scoped>
+  button {
+    background-color: brown;
+    color: aliceblue;
+  }
+</style>
