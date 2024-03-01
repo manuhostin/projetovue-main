81da8b7cc309faf301cdb0cd5637611039aadbb7
manuhostin <hostimmanu@gmail.com>
2024-03-01 13:55:34 -0300

    alterações

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+  import { ref } from 'vue';
 
-const contador = ref(0)
+const contador = ref(0);
 
-function decrementar () {
-  contador.value--
+function incrementar() {
+  contador.value++;
+}
+
+function decrementar() {
+  if (contador.value > 0) {
+    contador.value--;
+  }
+}
+
+function reiniciar() {
+  contador.value = 0;
 }
 
 </script>
 
 <template> 
-<button @click="contador++">+</button>
-<button @click="decrementar">-</button>
-<p>valor do contador: {{ contador }}</p>
+  <div>
+    <h1>Contador</h1>
+    <p> {{ contador }} </p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    <button @click="reiniciar">Reiniciar</button>
+  </div>
+
 </template>
 
 <style scoped>
